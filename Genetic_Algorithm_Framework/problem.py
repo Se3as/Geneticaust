63a00f31459cc4822b32e997c8903e449220b754
Luis Arias Gómez <luisalfredo.arias@ucr.ac.cr>
2025-06-25 23:46:35 -0600

    Abstract class for the problems with the minimal functions

---
+++ Genetic_Algorithm_Framework/problem.py
@@ -0,0 +1,11 @@
+from typing import List
+import random
+
+class Problem:
+
+    def fitness(self, chromosome: List[int]) -> float:
+        pass
+    def random_ind(self) -> List[int]:
+        pass
+    def heuristic_ind(self) -> List[int]:
+        pass
