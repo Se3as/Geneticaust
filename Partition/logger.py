d470f22df5f65224184d9d8629a09b0e64122798
Luis Arias Gómez <luisalfredo.arias@ucr.ac.cr>
2025-06-26 00:15:07 -0600

    Add item_sum in log for the complexity results

--- Partition/logger.py
+++ Partition/logger.py
@@ -5,13 +5,14 @@ class Log:
     def __init__(self, filename: str):
         self.filename = filename
         self.records = []
-        self.header = ["problem_name", "execution_time", "item_count"]
+        self.header = ["problem_name", "execution_time", "item_count", "item_sum"]
 
-    def add(self, problem_name: str, execution_time: float, item_count: int):
+    def add(self, problem_name: str, execution_time: float, item_count: int, item_sum: int):
         self.records.append({
             "problem_name": problem_name,
             "execution_time": execution_time,
-            "item_count": item_count
+            "item_count": item_count,
+            "item_sum": item_sum
         })
 
     def save(self):
