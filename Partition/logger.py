5bccce137e9a332e018ea7e9804296330e62708d
Luis Arias Gómez <luisalfredo.arias@ucr.ac.cr>
2025-06-26 20:35:37 -0600

    Define a seed so it is reproducible

--- Partition/logger.py
+++ Partition/logger.py
@@ -5,14 +5,14 @@ class Log:
     def __init__(self, filename: str):
         self.filename = filename
         self.records = []
-        self.header = ["problem_name", "execution_time", "item_count", "item_sum"]
+        self.header = ["problem", "time", "count", "sum"]
 
-    def add(self, problem_name: str, execution_time: float, item_count: int, item_sum: int):
+    def add(self, problem: str, time: float, count: int, sum: int):
         self.records.append({
-            "problem_name": problem_name,
-            "execution_time": execution_time,
-            "item_count": item_count,
-            "item_sum": item_sum
+            "problem": problem,
+            "time": time,
+            "count": count,
+            "sum": sum
         })
 
     def save(self):
--- Partition/main.py
+++ Partition/main.py
@@ -17,54 +17,56 @@ def generate_medium_case_list(n: int, max_value: int = 100) -> List[int]:
     return medium_list
 
 def run_test(
-    solver_instance: Any, 
-    test_name: str, 
-    data: List[int], 
+    method_test: Any, 
+    name: str, 
+    total_sum: List[int], 
     num_runs: int, 
     logger: Log
 ):
-    item_count: int = len(data)  
-    if "WorstCase" in test_name:
+    count: int = len(total_sum)  
+    if "WorstCase" in name:
         num_runs: int = 1
     for _ in range(num_runs):
         start_time: float = time.perf_counter()  
-        solver_instance.solve(data)
+        method_test.solve(total_sum)
         end_time: float = time.perf_counter()  
         duration: float = (end_time - start_time) * 1000000  
-        logger.add(test_name, duration, item_count, sum(data))
+        logger.add(name, duration, count, sum(total_sum))
 
 def main():
+    random.seed(777)
     N_ELEMENTS_WORSE: int = 26  
     NUM_RUNS: int = 2  
     NUM_RUNS_PER_LIST: int = 10  
     N_ELEMENTS_MEDIUM: int = 30  
     logger: Log = Log("partition.csv")  
     
-    solvers_to_test: List[tuple] = [
+    methods: List[tuple] = [
         (PartitionEx(), "PartitionExhaustive"), 
         (PartitionDP(), "PartitionDP")
     ]  
 
     worst_case_list: List[int] = generate_worst_case_list(N_ELEMENTS_WORSE)  
     
-    for solver, name in solvers_to_test:
+    for method, name in methods:
         run_test(
-            solver_instance=solver,
-            test_name=f"{name}_WorstCase",
-            data=worst_case_list,
+            method_test=method,
+            name=f"{name}_WorstCase",
+            total_sum=worst_case_list,
             num_runs=NUM_RUNS_PER_LIST,
             logger=logger
         )
-    for _ in range(NUM_RUNS):
+    for i in range(NUM_RUNS):
         medium_case_list: List[int] = generate_medium_case_list(N_ELEMENTS_MEDIUM)  
-        for solver, name in solvers_to_test:
+        for method, name in methods:
             run_test(
-                solver_instance=solver,
-                test_name=f"{name}_MediumCase",
-                data=medium_case_list,
+                method_test=method,
+                name=f"{name}_MediumCase",
+                total_sum=medium_case_list,
                 num_runs=NUM_RUNS_PER_LIST,
                 logger=logger
             )
+        random.seed(778+i)
 
     logger.save()
 
